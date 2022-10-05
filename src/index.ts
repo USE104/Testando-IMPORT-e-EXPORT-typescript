6df77f493f5975f501b9037c93c435df030726b9
JOSE HUGO DOS SANTOS SILVA <jose.hssilva4@senacsp.edu.br>
2022-10-05 11:47:15 -0300

    criando servidor com rotas estatica e dinamica

--- src/index.ts
+++ src/index.ts
@@ -28,9 +28,55 @@ console.log("subtrair"+Matematica.subtrair(n1,n2))
 console.log("multiplicar:"+Matematica.multiplicar(n1,n2))
 */
 
+/*
 import filmes from './filmes'
 
-
-
-
 console.log(filmes)
+*/
+
+
+// importanto o express
+import express from "express";
+import { Server } from "http";
+
+//usando express
+
+var server = express()
+
+// criando a porta de acesso
+
+server.listen(3000)
+
+// criando pagina web 
+server.get('/',(req,res)=>{
+    res.send("Hello wiston,cambio na escuta !")
+})
+
+//rota estatica
+
+// criando  a página de login
+server.get('/login',(req,res)=>{
+    res.send("Página de Login")
+})
+
+// rota dinamica
+server.get('/noticia/:politica',(req,res)=>{
+    let politica: string = req.params.politica
+    res.send("Noticiais: "+politica)
+})
+
+server.get('/noticia/:novidades',(req,res)=>{
+    let novidades: string = req.params.novidades
+    res.send("noticia: "+novidades)
+})
+
+
+server.get('/viagens/:origem-:destino',(req,res)=>{
+    var origem:string = req.params.origem
+    var destino: string = req.params.destino
+
+    res.send(`Procurando trajetos de ${origem} até ${destino}`)
+
+    
+
+})
